70db65e40ca8afa9c68ad71fea12509727fa2722
Medeiros <hsymetry@gmail.com>
2023-06-10 18:47:51 -0300

    Implement login and register page

--- lib/app/pages/login/login_page.dart
+++ lib/app/pages/login/login_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hero_software_test/app/controller/user_auth_controller.dart';
 import 'package:hero_software_test/app/core/helper/size_extensions.dart';
 import 'package:hero_software_test/app/core/styles/color_style.dart';
 import 'package:hero_software_test/app/core/styles/text_styles.dart';
@@ -7,6 +9,7 @@ import 'package:hero_software_test/app/core/widgets/container_logo.dart';
 import 'package:hero_software_test/app/core/widgets/custom_elevated_button.dart';
 import 'package:hero_software_test/app/core/widgets/custom_text_field.dart';
 import 'package:hero_software_test/app/pages/login/components/custom_checkbox.dart';
+import 'package:validatorless/validatorless.dart';
 
 import '../../core/widgets/custom_footer.dart';
 
@@ -20,7 +23,9 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _emailEC = TextEditingController();
   final _passwordEC = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   bool? isChecked = false;
+  bool passwordVisibility = false;
 
   //Transformando em stateful para usar o dispose nas controllers
   @override
@@ -32,81 +37,114 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final userAuthController = Get.put(UserAuthController());
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            // leading: Icon(
-            //   Icons.arrow_back,
-            //   color: context.colors.secondary,
-            // ),
             backgroundColor: context.colors.primary,
           ),
           body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const ContainerLogo(label: 'Acesse sua conta'),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'E-mail',
-                  hintText: 'E-mail',
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Senha',
-                  hintText: 'Senha',
-                  fieldWidth: context.percentWidth(.9),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(left: 25, right: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Lembrar-me',
-                        style: context.textStyles.textRegular
-                            .copyWith(fontSize: 17),
-                      ),
-                      CustomCheckbox(
-                        boolValue: isChecked,
-                        onChanged: (bool? newValue) {
-                          setState(() {
-                            print('Clicou na checkbox. Valor $newValue');
-                            isChecked = newValue!;
-                          });
-                        },
-                      ),
-                    ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ContainerLogo(label: ' Acesse sua conta'),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    controller: _emailEC,
+                    keyboardType: TextInputType.emailAddress,
+                    label: 'E-mail',
+                    hintText: 'E-mail',
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('E-mail obrigatório'),
+                      Validatorless.email('E-mail incorreto'),
+                    ]),
                   ),
-                ),
-                const SizedBox(height: 40),
-                CustomElevatedButton(
-                    onPressed: () => print('Clicou em entrar'),
-                    buttonTitle: 'ENTRAR'),
-                const SizedBox(height: 20),
-                Center(
-                  child: TextButton(
-                    onPressed: () => print('Clicou em esqueci minha senha'),
-                    child: Text(
-                      'Esqueci minha senha',
-                      style: context.textStyles.textMedium.copyWith(
-                          color: context.colors.secondary,
-                          fontSize: 17,
-                          decoration: TextDecoration.underline),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    controller: _passwordEC,
+                    keyboardType: TextInputType.emailAddress,
+                    label: 'Senha',
+                    hintText: 'Senha',
+                    fieldWidth: context.percentWidth(.9),
+                    obscureText: !passwordVisibility,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          passwordVisibility = !passwordVisibility;
+                        });
+                      },
+                      child: Icon(
+                        passwordVisibility == true
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        color: Colors.grey.shade500,
+                        size: 22,
+                      ),
+                    ),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatória'),
+                      Validatorless.min(6, 'Mínimo de 6 caracteres requerido'),
+                    ]),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Lembrar-me',
+                          style: context.textStyles.textRegular
+                              .copyWith(fontSize: 17),
+                        ),
+                        CustomCheckbox(
+                          boolValue: isChecked,
+                          onChanged: (bool? newValue) {
+                            setState(() {
+                              print('Clicou na checkbox. Valor $newValue');
+                              isChecked = newValue!;
+                            });
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                CustomFooter(
-                    onPressed: () => print('Clicou para cadastrar-se'),
-                    label: 'Novo por aqui? ',
-                    buttonLabel: 'CADASTRE-SE'),
-              ],
+                  const SizedBox(height: 40),
+                  CustomElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          userAuthController.loginWithEmailAndPassword(
+                              emailAddress: _emailEC.text.trim(),
+                              password: _passwordEC.text.trim());
+                          Get.offNamed('/home');
+                        }
+                      },
+                      buttonTitle: 'ENTRAR'),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: TextButton(
+                      onPressed: () => Get.toNamed('/email'),
+                      child: Text(
+                        'Esqueci minha senha',
+                        style: context.textStyles.textMedium.copyWith(
+                            color: context.colors.secondary,
+                            fontSize: 17,
+                            decoration: TextDecoration.underline),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  CustomFooter(
+                      onPressed: () => Get.toNamed('/register'),
+                      label: 'Novo por aqui?',
+                      buttonLabel: 'CADASTRE-SE'),
+                ],
+              ),
             ),
           ),
         ),
--- lib/app/pages/register/register_page.dart
+++ lib/app/pages/register/register_page.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hero_software_test/app/controller/user_auth_controller.dart';
 import 'package:hero_software_test/app/core/helper/size_extensions.dart';
 import 'package:hero_software_test/app/core/styles/color_style.dart';
 import 'package:hero_software_test/app/core/styles/text_styles.dart';
 import 'package:hero_software_test/app/core/widgets/container_logo.dart';
 import 'package:hero_software_test/app/core/widgets/custom_elevated_button.dart';
 import 'package:hero_software_test/app/core/widgets/custom_text_field.dart';
+import 'package:validatorless/validatorless.dart';
 
 import '../../core/widgets/custom_footer.dart';
+import 'components/custom_date_of_birth_component.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final userAuthController = Get.put(UserAuthController());
+  bool passwordVisibility = false;
+  bool confirmPasswordVisibility = false;
+  final formKey = GlobalKey<FormState>();
+
+  final _usernameEC = TextEditingController();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+  final _confirmPasswordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameEC.dispose();
+    _emailEC.dispose();
+    _passwordEC.dispose();
+    _confirmPasswordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,153 +49,191 @@ class RegisterPage extends StatelessWidget {
             backgroundColor: context.colors.primary,
           ),
           body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const ContainerLogo(
-                    label:
-                        'Olá, que bom ter você aqui!\nBem-vindo ao CARROCASH!'),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Nome completo',
-                  hintText: 'Nome completo',
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'CPF',
-                  hintText: 'CPF',
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text.rich(
-                    TextSpan(
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ContainerLogo(
+                      label:
+                          ' Olá, que bom ter você aqui!\n Bem-vindo ao CARROCASH!'),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    controller: _usernameEC,
+                    keyboardType: TextInputType.name,
+                    label: 'Nome completo',
+                    hintText: 'Nome completo',
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('Nome obrigatório'),
+                      Validatorless.min(3, 'Mínimo requirido de 3 caracteres')
+                    ]),
+                  ),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    keyboardType: TextInputType.number,
+                    label: 'CPF',
+                    hintText: 'CPF',
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('CPF obrigatório'),
+                      Validatorless.cpf('CPF inválido'),
+                    ]),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Data de nascimento',
+                            style: context.textStyles.textMedium.copyWith(
+                              color: context.colors.secondary,
+                              fontSize: 17,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ' *',
+                            style: context.textStyles.textExtraBold.copyWith(
+                              color: context.colors.blue,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextSpan(
-                          text: 'Data de nascimento',
-                          style: context.textStyles.textMedium.copyWith(
-                            color: context.colors.secondary,
-                            fontSize: 17,
-                          ),
+                        CustomDateOfBirthComponent(
+                          hintText: 'DD',
+                          fieldWidth: context.percentWidth(.25),
+                          validation: Validatorless.required('Obrigatório'),
                         ),
-                        TextSpan(
-                          text: ' *',
-                          style: context.textStyles.textExtraBold.copyWith(
-                            color: context.colors.blue,
-                            fontSize: 17,
-                          ),
+                        CustomDateOfBirthComponent(
+                          hintText: 'MM',
+                          fieldWidth: context.percentWidth(.25),
+                          validation: Validatorless.required('Obrigatório'),
+                        ),
+                        CustomDateOfBirthComponent(
+                          hintText: 'AAAA',
+                          fieldWidth: context.percentWidth(.25),
+                          validation: Validatorless.required('Obrigatório'),
                         ),
                       ],
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CustomDateOfBirth(
-                          hintText: 'DD',
-                          fieldWidth: context.percentWidth(.25)),
-                      CustomDateOfBirth(
-                          hintText: 'MM',
-                          fieldWidth: context.percentWidth(.25)),
-                      CustomDateOfBirth(
-                          hintText: 'AAAA',
-                          fieldWidth: context.percentWidth(.25)),
-                    ],
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    keyboardType: TextInputType.phone,
+                    label: 'Celular',
+                    hintText: 'DDD + Celular',
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('Número obrigatório'),
+                    ]),
                   ),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'Celular',
-                  hintText: 'DDD + Celular',
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  label: 'E-mail',
-                  hintText: 'E-mail',
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField.password(
-                  label: 'Senha',
-                  hintText: 'Mínimo 6 caracteres',
-                  fontSize: 17,
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField.password(
-                  label: 'Confirma senha',
-                  hintText: 'Mínimo 6 caracteres',
-                  fontSize: 17,
-                  fieldWidth: context.percentWidth(.9),
-                ),
-                const SizedBox(height: 40),
-                CustomElevatedButton(
-                    onPressed: () => print('Clicou em próximo'),
-                    buttonTitle: 'Próximo'),
-                const SizedBox(height: 40),
-                CustomFooter(
-                    onPressed: () => print('Clicou para voltar ao login'),
-                    label: 'Possui uma conta? ',
-                    buttonLabel: 'ENTRAR'),
-                const SizedBox(height: 10),
-              ],
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    controller: _emailEC,
+                    keyboardType: TextInputType.emailAddress,
+                    label: 'E-mail',
+                    hintText: 'E-mail',
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('Email obrigatório'),
+                      Validatorless.email('E-mail inválido')
+                    ]),
+                  ),
+                  const SizedBox(height: 20),
+                  CustomTextField.password(
+                    controller: _passwordEC,
+                    obscureText: !passwordVisibility,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          passwordVisibility = !passwordVisibility;
+                        });
+                      },
+                      child: Icon(
+                        passwordVisibility == true
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        color: Colors.grey.shade500,
+                        size: 22,
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    label: 'Senha',
+                    hintText: 'Mínimo 6 caracteres',
+                    fontSize: 17,
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatório'),
+                      Validatorless.min(6, 'Mínimo de 6 caracteres'),
+                    ]),
+                  ),
+                  const SizedBox(height: 20),
+                  CustomTextField.password(
+                    controller: _confirmPasswordEC,
+                    obscureText: !confirmPasswordVisibility,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          confirmPasswordVisibility =
+                              !confirmPasswordVisibility;
+                        });
+                      },
+                      child: Icon(
+                        passwordVisibility == true
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        color: Colors.grey.shade500,
+                        size: 22,
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    label: 'Confirma senha',
+                    hintText: 'Mínimo 6 caracteres',
+                    fontSize: 17,
+                    fieldWidth: context.percentWidth(.9),
+                    validation: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatório'),
+                      Validatorless.min(6, 'Mínimo de 6 caracteres'),
+                      Validatorless.compare(
+                          _passwordEC, 'As senhas não coincidem'),
+                    ]),
+                  ),
+                  const SizedBox(height: 40),
+                  CustomElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          userAuthController.createAccount(
+                            username: _usernameEC.text.trim(),
+                            emailAddress: _emailEC.text.trim(),
+                            password: _passwordEC.text.trim(),
+                          );
+                          Get.offNamed('/home');
+                        } else {
+                          Get.offNamed('/login');
+                        }
+                      },
+                      buttonTitle: 'Próximo'),
+                  const SizedBox(height: 40),
+                  CustomFooter(
+                      onPressed: () => Get.toNamed('/login'),
+                      label: 'Possui uma conta?',
+                      buttonLabel: 'ENTRAR'),
+                  const SizedBox(height: 10),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class CustomDateOfBirth extends StatelessWidget {
-  final String hintText;
-  final double fieldWidth;
-  final String? Function(String?)? validation;
-  final bool obscureText;
-  final double fontSize;
-
-  const CustomDateOfBirth({
-    super.key,
-    this.validation,
-    this.obscureText = false,
-    required this.fieldWidth,
-    this.fontSize = 17,
-    required this.hintText,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 10),
-          SizedBox(
-            width: fieldWidth,
-            child: TextFormField(
-              cursorColor: context.colors.secondary,
-              cursorHeight: 20,
-              style: context.textStyles.textRegular.copyWith(
-                  fontSize: fontSize,
-                  color: context.colors.secondary,
-                  letterSpacing: 1),
-              obscureText: obscureText,
-              decoration: InputDecoration(
-                hintText: hintText,
-                hintStyle: context.textStyles.textRegular.copyWith(
-                  color: Colors.grey.shade400,
-                ),
-              ),
-              validator: validation,
-            ),
-          ),
-        ],
       ),
     );
   }
