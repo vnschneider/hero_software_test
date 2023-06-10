5b9b836f29848c9880c396a0ffe93246bb5ea2b3
Medeiros <hsymetry@gmail.com>
2023-06-10 18:47:37 -0300

    Add named routes

---
+++ lib/app/core/routes/named_routes.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hero_software_test/app/pages/home/home_page.dart';
+import 'package:hero_software_test/app/pages/recover/email_confirmation_page.dart';
+import 'package:hero_software_test/app/pages/recover/password_confirmation_page.dart';
+import 'package:hero_software_test/app/pages/register/register_page.dart';
+import 'package:hero_software_test/app/pages/splash/splash_page.dart';
+
+import '../../pages/login/login_page.dart';
+
+List<GetPage<dynamic>>? getPages = [
+    GetPage(
+    name: '/',
+    page: () => const SplashPage(),
+    curve: Curves.easeIn,
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+  GetPage(
+    name: '/login',
+    page: () => const LoginPage(),
+    curve: Curves.easeIn,
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+  GetPage(
+    name: '/register',
+    page: () => const RegisterPage(),
+    curve: Curves.easeIn,
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+  GetPage(
+    name: '/home',
+    page: () => const HomePage(),
+    curve: Curves.easeIn,
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+  GetPage(
+    name: '/email',
+    page: () => const EmailConfirmationPage(),
+    curve: Curves.easeIn,
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+  GetPage(
+    name: '/password',
+    page: () => const PasswordConfirmationPage(),
+    curve: Curves.easeIn,
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+];
