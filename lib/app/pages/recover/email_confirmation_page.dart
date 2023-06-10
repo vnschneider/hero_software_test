b2f8be0199ad1dfb0604a56645064482c676123b
Medeiros <hsymetry@gmail.com>
2023-06-10 18:47:26 -0300

    Add confirmation pages

--- lib/app/pages/recover/email_confirmation_page.dart
+++ lib/app/pages/recover/email_confirmation_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hero_software_test/app/core/helper/size_extensions.dart';
 import 'package:hero_software_test/app/core/styles/color_style.dart';
 import 'package:hero_software_test/app/core/styles/text_styles.dart';
 import 'package:hero_software_test/app/core/widgets/custom_elevated_button.dart';
 import 'package:hero_software_test/app/core/widgets/custom_text_field.dart';
+import 'package:validatorless/validatorless.dart';
 
-class EmailRecoveryPage extends StatelessWidget {
-  const EmailRecoveryPage({super.key});
+class EmailConfirmationPage extends StatelessWidget {
+  const EmailConfirmationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +31,22 @@ class EmailRecoveryPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 20),
                 Text(
-                  'Digite seu e-mail cadastrado para receber as instruções de redefinição. Fique tranquilo, você poderá alterá-la em poucos passos.',
+                  'Digite o e-mail cadastrado para receber as instruções de redefinição. Fique tranquilo, você poderá alterá-la em poucos passos.',
                   style: context.textStyles.textRegular
                       .copyWith(fontSize: 18, color: Colors.grey.shade600),
                 ),
                 const SizedBox(height: 20),
                 CustomTextField(
-                    isMandatory: false,
-                    fieldWidth: context.percentWidth(.9),
-                    hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  isMandatory: false,
+                  fieldWidth: context.percentWidth(.9),
+                  hintText: 'E-mail',
+                  validation: Validatorless.email('E-mail inválido'),
+                ),
                 const Spacer(),
                 CustomElevatedButton(
-                    onPressed: () {}, buttonTitle: 'PRÓXIMO'),
+                    onPressed: () => Get.toNamed('/password'),
+                    buttonTitle: 'PRÓXIMO'),
                 const SizedBox(height: 20),
               ],
             ),
--- lib/app/pages/recover/password_confirmation_page.dart
+++ lib/app/pages/recover/password_confirmation_page.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:hero_software_test/app/core/helper/size_extensions.dart';
+import 'package:get/get.dart';
+import 'package:hero_software_test/app/controller/user_auth_controller.dart';
 import 'package:hero_software_test/app/core/styles/color_style.dart';
 import 'package:hero_software_test/app/core/styles/text_styles.dart';
-import 'package:hero_software_test/app/core/widgets/container_logo.dart';
 import 'package:hero_software_test/app/core/widgets/custom_elevated_button.dart';
-import 'package:hero_software_test/app/core/widgets/custom_text_field.dart';
 
-class PasswordConfirmationPage extends StatelessWidget {
+class PasswordConfirmationPage extends StatefulWidget {
   const PasswordConfirmationPage({super.key});
 
   @override
+  State<PasswordConfirmationPage> createState() =>
+      _PasswordConfirmationPageState();
+}
+
+class _PasswordConfirmationPageState extends State<PasswordConfirmationPage> {
+  bool passwordVisibility = false;
+  bool confirmPasswordVisibility = false;
+  final _emailEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailEC.dispose;
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final userAuthController = Get.put(UserAuthController());
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: SafeArea(
@@ -24,25 +40,25 @@ class PasswordConfirmationPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Digite sua nova senha',
+                  'Quase tudo pronto!',
                   style: context.textStyles.textTitle
                       .copyWith(color: context.colors.secondary),
                 ),
-                const SizedBox(height: 20),                
-                CustomTextField(
-                  label: 'Digite sua nova senha',
-                    isMandatory: false,
-                    fieldWidth: context.percentWidth(.9),
-                    hintText: 'Senha'),
-                    const SizedBox(height: 20),
-                    CustomTextField(
-                    label: 'Digite novamente',
-                    isMandatory: false,
-                    fieldWidth: context.percentWidth(.9),
-                    hintText: 'Senha'),
+                const SizedBox(height: 20),
+                Text(
+                  'Cheque sua caixa de e-mails e siga os passos para alterar sua senha. Em seguida, faça o login novamente na nossa plataforma.',
+                  style: context.textStyles.textRegular
+                      .copyWith(fontSize: 18, color: Colors.grey.shade600),
+                ),
+                const SizedBox(height: 20),
                 const Spacer(),
                 CustomElevatedButton(
-                    onPressed: () {}, buttonTitle: 'CONFIRMAR'),
+                    onPressed: () {
+                      userAuthController.resetPassword(
+                          email: _emailEC.text.trim());
+                      Get.offNamed('/login');
+                    },
+                    buttonTitle: 'CONFIRMAR'),
                 const SizedBox(height: 20),
               ],
             ),
