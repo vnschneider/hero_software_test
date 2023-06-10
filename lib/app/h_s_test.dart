2f5904a78a45d05cf57f17bacdd13dc6a15ef4da
Medeiros <hsymetry@gmail.com>
2023-06-10 18:48:19 -0300

    Add firebase

--- lib/app/h_s_test.dart
+++ lib/app/h_s_test.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hero_software_test/app/core/routes/named_routes.dart';
 import 'package:hero_software_test/app/core/styles/theme_style.dart';
-import 'package:hero_software_test/app/pages/login/login_page.dart';
+import 'package:hero_software_test/app/pages/splash/splash_page.dart';
 
 class HSTest extends StatelessWidget {
   const HSTest({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeStyle.theme,
-      home: const LoginPage(),
+      home: const SplashPage(),
+      getPages: getPages,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hero_software_test/app/controller/user_auth_controller.dart';
 
 import 'app/h_s_test.dart';
+import 'firebase_options.dart';
 
-void main(List<String> args) {
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((value) {
+    Get.put(UserAuthController());
+    
+  });
   runApp(const HSTest());
 }
